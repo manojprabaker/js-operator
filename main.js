f9cec56f3efd82b39d1eb6e56aad7734d1335b3b
manojprabaker <manojprabaker999@gmail.com>
2023-02-24 09:17:26 +0530

    add

---
+++ main.js
@@ -0,0 +1,94 @@
+document.write("hai");
+
+var a=5, b=8, c=4;
+console.log(a+b-c);
+
+console.log(a+b*c);
+
+console.log(a*b/c);
+
+console.log(a-b/c);
+
+console.log(b%c-a);
+
+console.log(a*b%c);
+
+console.log(a**c/b);
+
+console.log(a**c-b);
+
+console.log(a**c-b/c);
+
+console.log(a-b*c);
+
+console.log(-(b**c));
+
+console.log(a++);
+
+console.log(--a);
+
+console.log(b%a**c);
+
+console.log(b++ - a**c);
+
+console.log(b++ - a**c);
+
+var x=2;
+x += 3;
+console.log(x);
+
+var p=4,q=4;
+console.log(p == q);
+
+var p="4",q=4;
+console.log(p === q);
+
+var p="mj",q=4;
+console.log(p !== q);
+
+var p=4,q=4;
+console.log(--p);
+
+var p1="fg",q1=87;
+console.log(p1==q1);
+
+var p1="fg",q1="fg";
+console.log(p1!=q1);
+
+var p2="fg",q2="fgh";
+console.log(p2==q2);
+
+
+var m=3; n=4;
+console.log(m);
+m=n;
+console.log(m);
+
+var p3="mj" , q3="8";
+console.log(p3<=q3);
+
+
+var p4="cv" , q4=8;
+console.log(p4==q4);
+
+
+var a=0,b=2,c=3,d=4,e=5,f=6;
+console.log(b/a);
+
+var a=2,b=3,c=4;
+console.log(a%b/c);
+
+var a=2,b=3,c=5;
+console.log(b*c/a%b);
+
+var a=5,b=18,c=4,d=3,e=2;
+console.log(a+b**c-d%e/a*d);
+
+var a=2,b=5,c=3,d=4;
+console.log(a**b+c-d*a%c/d);
+
+var a=20,b=2;
+console.log((a++)+(++a)-(a+b));
+
+var a=1,b=7,c=3;
+console.log((a*b)&&(c*a));
